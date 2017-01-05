3b1fe8e63e99aa1884695cf89b60be3204e3a0dc
Sebastian Probst Eide <sebastian@aircloak.com>
2017-01-05 12:40:30 +0100

    Silence warning produced by Elixir 1.4.0

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Phoenix.GenSocketClient.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
+      deps: deps(),
       docs: [
         main: "readme",
         extras: ["README.md"]
