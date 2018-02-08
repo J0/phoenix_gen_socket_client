44a5b95957abb3cf03ba6c60f3a70c1d47c7b69c
Sasa Juric <sasa.juric@gmail.com>
2018-02-08 10:38:51 +0100

    upgrade credo

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Phoenix.GenSocketClient.Mixfile do
       {:poison, "~> 2.0 or ~> 3.0", optional: true},
       {:phoenix, "~> 1.3", only: :test},
       {:cowboy, "~> 1.0", only: :test},
-      {:credo, "~> 0.3.0", only: [:dev, :test]},
+      {:credo, "~> 0.8.10", runtime: false},
       {:dialyze, "~> 0.2.1", only: :dev},
       {:ex_doc, "~> 0.17.1", only: :docs}
     ]
